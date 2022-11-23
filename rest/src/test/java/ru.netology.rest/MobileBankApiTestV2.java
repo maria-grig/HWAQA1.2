41e27e322620bdc8bb880d9b2b9e39a2b11a154c
MariaG <MGrigorenko@bellintegrator.com>
2022-11-24 00:26:36 +0300

    refactor

--- rest/src/test/java/ru.netology.rest/MobileBankApiTestV2.java
+++ rest/src/test/java/ru.netology.rest/MobileBankApiTestV2.java
@@ -1,5 +1,6 @@
 package ru.netology.rest;
 
+import io.restassured.http.ContentType;
 import org.junit.jupiter.api.Test;
 
 import static io.restassured.RestAssured.given;
@@ -16,11 +17,11 @@ class MobileBankApiTestV2 {
       .when()
           .get("/demo/accounts")
       // Проверки
-      .then()
-          .statusCode(200)
-          // .header("Content-Type", "application/json; charset=UTF-8")
-          // специализированные проверки - лучше
-          .contentType(ContentType.JSON)
-      ;
+              .then()
+              .statusCode(200)
+              .body(matchesJsonSchemaInClasspath("accounts.schema.json"))
+              .header("Content-Type", "application/json; charset=UTF-8")
+              // специализированные проверки - лучше
+              .contentType(ContentType.JSON);
     }
 }
