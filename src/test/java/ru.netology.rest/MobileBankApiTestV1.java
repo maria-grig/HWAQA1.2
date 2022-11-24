528461ea142ec3a4d2dd96c7fe589b16c7be8b82
MariaG <MGrigorenko@bellintegrator.com>
2022-11-24 12:07:20 +0300

    modified test2

--- src/test/java/ru.netology.rest/MobileBankApiTestV1.java
+++ src/test/java/ru.netology.rest/MobileBankApiTestV1.java
@@ -3,7 +3,6 @@ package ru.netology.rest;
 import org.junit.jupiter.api.Test;
 
 import static io.restassured.RestAssured.given;
-import static io.restassured.module.jsv.JsonSchemaValidator.matchesJsonSchemaInClasspath;
 
 class MobileBankApiTestV1 {
     @Test
